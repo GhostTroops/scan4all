1063da30f99363fd69a3af35011a25c371174299
veo <38176360+veo@users.noreply.github.com>
2021-07-05 15:30:39 +0800

    update weblogic_brute

--- pkg/brute/weblogic_brute.go
+++ pkg/brute/weblogic_brute.go
@@ -4,16 +4,19 @@ import "fmt"
 
 func Weblogic_brute(url string) (username string, password string) {
 	if req, err := httpRequset(url+"/console/", "GET", ""); err == nil {
+		locationlogin, _ := req.Location()
 		if req.StatusCode == 302 {
 			for uspa := range weblogicuserpass {
 				if req2, err2 := httpRequset(url+"/console/j_security_check", "POST", fmt.Sprintf("j_username=%s&j_password=%s", weblogicuserpass[uspa].username, weblogicuserpass[uspa].password)); err2 == nil {
-					if req2.ContentLength != req.ContentLength {
+					location, _ := req2.Location()
+					if location != locationlogin {
 						fmt.Printf("weblogic-brute-sucess|%s:%s--%s", weblogicuserpass[uspa].username, weblogicuserpass[uspa].password, url)
 						fmt.Println()
 						return weblogicuserpass[uspa].username, weblogicuserpass[uspa].password
 					}
 				}
 			}
+			return "login_page", ""
 		}
 	}
 	return "", ""
