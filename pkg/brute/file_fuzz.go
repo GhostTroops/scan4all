fddc4bd7a3d8acdd2ff2e43c049fae43e62abc05
veo <38176360+veo@users.noreply.github.com>
2021-07-09 16:02:08 +0800

    fix bug

--- pkg/brute/file_fuzz.go
+++ pkg/brute/file_fuzz.go
@@ -1,12 +1,9 @@
 package brute
 
-import "fmt"
-
 func File_fuzz(url string) (path []string) {
 	if _, err := httpRequset(url, "GET", ""); err == nil {
 		for urli := range filedic {
 			if req2, err := httpRequset(url+filedic[urli], "GET", ""); err == nil {
-				fmt.Println(req2.StatusCode)
 				if req2.StatusCode == 200 {
 					path = append(path, url+filedic[urli])
 				}
