6e1b6a5cf10a638a487f65f3d09cb284b8a0e690
veo <38176360+veo@users.noreply.github.com>
2021-07-13 13:16:21 +0800

    优化 fuzz

--- pkg/brute/file_fuzz.go
+++ pkg/brute/file_fuzz.go
@@ -1,9 +1,9 @@
 package brute
 
 func File_fuzz(url string) (path []string) {
-	if _, err := httpRequset(url, "GET", ""); err == nil {
+	if _, err := httpRequset(url, "HEAD", ""); err == nil {
 		for urli := range filedic {
-			if req2, err := httpRequset(url+filedic[urli], "GET", ""); err == nil {
+			if req2, err := httpRequset(url+filedic[urli], "HEAD", ""); err == nil {
 				if req2.StatusCode == 200 {
 					path = append(path, url+filedic[urli])
 				}
--- pkg/exp/shiro/check.go
+++ pkg/exp/shiro/check.go
@@ -99,7 +99,7 @@ func findTheKey(key_len int, url string, Shirokeys string, Content []byte, keyle
 }
 
 func getCommandArgs() {
-	userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"
+	userAgent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
 	method = "GET"
 	postContent = ""
 	timeout = 5
