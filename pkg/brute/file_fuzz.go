770d237ec0447c01f38151b717e66a88f2c007db
veo <38176360+veo@users.noreply.github.com>
2021-07-14 13:07:50 +0800

    优化误报

--- pkg/brute/file_fuzz.go
+++ pkg/brute/file_fuzz.go
@@ -1,10 +1,14 @@
 package brute
 
 func File_fuzz(url string) (path []string) {
+
 	if _, err := httpRequset(url, "HEAD", ""); err == nil {
 		for urli := range filedic {
+			lastword := filedic[urli][len(filedic[urli])-1:]
 			if req2, err := httpRequset(url+filedic[urli], "HEAD", ""); err == nil {
-				if req2.StatusCode == 200 || req2.StatusCode == 403 {
+				if lastword == "/" && (req2.StatusCode == 200 || req2.StatusCode == 403) {
+					path = append(path, url+filedic[urli])
+				} else if req2.StatusCode == 200 {
 					path = append(path, url+filedic[urli])
 				}
 			}
