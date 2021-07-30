e6b9769e35513bf7b19581632e42c2e3bd0bec66
veo <38176360+veo@users.noreply.github.com>
2021-07-30 17:20:05 +0800

    降低敏感文件扫描误报率，基本不再误报

--- pkg/brute/file_fuzz.go
+++ pkg/brute/file_fuzz.go
@@ -1,13 +1,54 @@
 package brute
 
-func File_fuzz(url string) (path []string) {
+var dirpath []string
+var filepath []string
 
+func File_fuzz(url string) (path []string) {
 	if req, err := httpRequset(url+"/url_not_support/", "HEAD", ""); err == nil {
-		if req.StatusCode != 403 {
+		if req.StatusCode == 404 {
 			for urli := range filedic {
 				lastword := filedic[urli][len(filedic[urli])-1:]
 				if req2, err := httpRequset(url+filedic[urli], "HEAD", ""); err == nil {
-					if lastword == "/" && (req2.StatusCode == 302 || req2.StatusCode == 403) {
+					if lastword == "/" && (req2.StatusCode == 302 || req2.StatusCode == 403 || req2.StatusCode == 200) {
+						path = append(path, url+filedic[urli])
+					} else if req2.StatusCode == 200 {
+						path = append(path, url+filedic[urli])
+					}
+				}
+			}
+		} else if req.StatusCode == 200 || req.StatusCode == 302 {
+			for urli := range filedic {
+				lastword := filedic[urli][len(filedic[urli])-1:]
+				if req2, err := httpRequset(url+filedic[urli], "HEAD", ""); err == nil {
+					if lastword == "/" {
+						dirpath = append(dirpath, url+filedic[urli])
+					} else {
+						filepath = append(filepath, url+filedic[urli])
+					}
+					if lastword == "/" && (req2.StatusCode == 403) {
+						path = append(path, url+filedic[urli])
+					} else if req2.StatusCode == 200 {
+						path = append(path, url+filedic[urli])
+					}
+				}
+			}
+			if len(filepath) == len(path) {
+				path = nil
+			}
+		} else if req.StatusCode == 403 {
+			for urli := range filedic {
+				if req2, err := httpRequset(url+filedic[urli], "HEAD", ""); err == nil {
+					if req2.StatusCode == 200 {
+						path = append(path, url+filedic[urli])
+					}
+				}
+			}
+
+		} else {
+			for urli := range filedic {
+				lastword := filedic[urli][len(filedic[urli])-1:]
+				if req2, err := httpRequset(url+filedic[urli], "HEAD", ""); err == nil {
+					if lastword == "/" && (req2.StatusCode == 302 || req2.StatusCode == 403 || req2.StatusCode == 200) {
 						path = append(path, url+filedic[urli])
 					} else if req2.StatusCode == 200 {
 						path = append(path, url+filedic[urli])
