ac599726db7a33c07f5c604550593676d8159d14
veo <38176360+veo@users.noreply.github.com>
2021-08-02 16:38:44 +0800

    完善weblogic爆破

--- brute/file_fuzz.go
+++ brute/file_fuzz.go
@@ -9,10 +9,10 @@ func File_fuzz(url string) (path []string) {
 			for urli := range filedic {
 				lastword := filedic[urli][len(filedic[urli])-1:]
 				if req2, err := httpRequset(url+filedic[urli], "HEAD", ""); err == nil {
-					if lastword == "/" && (req2.StatusCode == 302 || req2.StatusCode == 403 || req2.StatusCode == 200) {
-						path = append(path, url+filedic[urli])
-					} else if req2.StatusCode == 200 {
-						path = append(path, url+filedic[urli])
+					if lastword == "/" && (req2.StatusCode == 302 || req2.StatusCode == 403 || req2.StatusCode == 200 || req2.StatusCode == 401) {
+						path = append(path, filedic[urli])
+					} else if req2.StatusCode == 200 || req2.StatusCode == 401 {
+						path = append(path, filedic[urli])
 					}
 				}
 			}
@@ -21,14 +21,14 @@ func File_fuzz(url string) (path []string) {
 				lastword := filedic[urli][len(filedic[urli])-1:]
 				if req2, err := httpRequset(url+filedic[urli], "HEAD", ""); err == nil {
 					if lastword == "/" {
-						dirpath = append(dirpath, url+filedic[urli])
+						dirpath = append(dirpath, filedic[urli])
 					} else {
-						filepath = append(filepath, url+filedic[urli])
+						filepath = append(filepath, filedic[urli])
 					}
 					if lastword == "/" && (req2.StatusCode == 403) {
-						path = append(path, url+filedic[urli])
+						path = append(path, filedic[urli])
 					} else if req2.StatusCode == 200 {
-						path = append(path, url+filedic[urli])
+						path = append(path, filedic[urli])
 					}
 				}
 			}
@@ -39,7 +39,7 @@ func File_fuzz(url string) (path []string) {
 			for urli := range filedic {
 				if req2, err := httpRequset(url+filedic[urli], "HEAD", ""); err == nil {
 					if req2.StatusCode == 200 {
-						path = append(path, url+filedic[urli])
+						path = append(path, filedic[urli])
 					}
 				}
 			}
@@ -48,10 +48,10 @@ func File_fuzz(url string) (path []string) {
 			for urli := range filedic {
 				lastword := filedic[urli][len(filedic[urli])-1:]
 				if req2, err := httpRequset(url+filedic[urli], "HEAD", ""); err == nil {
-					if lastword == "/" && (req2.StatusCode == 302 || req2.StatusCode == 403 || req2.StatusCode == 200) {
-						path = append(path, url+filedic[urli])
-					} else if req2.StatusCode == 200 {
-						path = append(path, url+filedic[urli])
+					if lastword == "/" && (req2.StatusCode == 302 || req2.StatusCode == 403 || req2.StatusCode == 200 || req2.StatusCode == 401) {
+						path = append(path, filedic[urli])
+					} else if req2.StatusCode == 200 || req2.StatusCode == 401 {
+						path = append(path, filedic[urli])
 					}
 				}
 			}
