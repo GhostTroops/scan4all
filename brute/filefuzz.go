3e714077c1a5e63cf14a27662d82d7b3f6c5a65b
veo <38176360+veo@users.noreply.github.com>
2021-08-30 17:33:38 +0800

    优化敏感文件扫描

--- brute/filefuzz.go
+++ brute/filefuzz.go
@@ -4,6 +4,7 @@ import (
 	"github.com/veo/vscan/pkg"
 	"regexp"
 	"strings"
+	"time"
 )
 
 type page struct {
@@ -36,7 +37,7 @@ func reqPage(u string) (*page, *pkg.Response, error) {
 		}
 	}
 	if req, err := pkg.HttpRequset(u, method, "", false, nil); err == nil {
-		if pkg.In_slice_int(req.StatusCode, []int{301, 302, 307, 308}) {
+		if pkg.IntInSlice(req.StatusCode, []int{301, 302, 307, 308}) {
 			page.is302 = true
 		}
 		page.title = gettitle(req.Body)
@@ -60,8 +61,8 @@ func reqPage(u string) (*page, *pkg.Response, error) {
 func FileFuzz(u string, indexStatusCode int, indexContentLength int, indexbody string) (path []string, technologies []string) {
 	var path404 = "/file_not_support"
 	var page200CodeList = []int{200, 301, 302, 401, 500}
-	var page404Title = []string{"404", "不存在", "错误", "403", "禁止访问", "请求含有不合法的参数", "网络防火墙", "访问拦截", "由于安全原因JSP功能默认关闭"}
-	var page404Content = []string{"<script>document.getElementById(\"a-link\").click();</script>"}
+	var page404Title = []string{"404", "不存在", "错误", "403", "禁止访问", "请求含有不合法的参数", "网络防火墙", "网站防火墙", "访问拦截", "由于安全原因JSP功能默认关闭"}
+	var page404Content = []string{"<script>document.getElementById(\"a-link\").click();</script>", "404 Not Found", "您所提交的请求含有不合法的参数，已被网站管理员设置拦截"}
 	var page403title = []string{"403", "Forbidden", "ERROR"}
 	var page403Content = []string{"403", "Forbidden", "ERROR"}
 	var location404 = []string{"/auth/login/", "error.html"}
@@ -83,7 +84,7 @@ func FileFuzz(u string, indexStatusCode int, indexContentLength int, indexbody s
 		if url404.is302 {
 			location404 = append(location404, url404.locationUrl)
 		}
-		if url404.is302 && strings.HasSuffix(url404.locationUrl, url404req.RequestUrl+"/file_not_support/") {
+		if url404.is302 && strings.HasSuffix(url404.locationUrl, "/file_not_support/") {
 			skip302 = true
 		}
 		if url404.is403 || indexStatusCode == 403 {
@@ -96,122 +97,123 @@ func FileFuzz(u string, indexStatusCode int, indexContentLength int, indexbody s
 	}
 	//多线程实测效果并不理想，不使用
 	//var wg sync.WaitGroup
-	//ch := make(chan struct{}, 20)
+	ch := make(chan struct{}, 20)
+
 	for _, payload := range filedic {
 		var is404Page = false
 		if errorTimes > 20 {
 			return
 		}
-		//ch <- struct{}{}
-		//wg.Add(1)
-		//go func(payloadx string) {
-		if url, req, err := reqPage(u + payload); err == nil {
-			if url.is403 && (pkg.In_slice_string(url.title, page403title) || pkg.In_slice_string(req.Body, page403Content)) && !skip403 {
-				// 基于403页面文件扫描指纹添加
-				switch payload {
-				case "/Runtime/Logs/":
-					technologies = append(technologies, "ThinkPHP")
-				}
-				path = append(path, payload)
-			}
-			if !pkg.In_slice_int(req.StatusCode, page200CodeList) {
-				is404Page = true
-			}
-			if url.isBackUpPath {
-				if !url.isBackUpPage {
-					is404Page = true
-				}
-			}
-			if pkg.In_slice_string(url.title, page404Title) {
-				is404Page = true
-			}
-			if pkg.In_slice_string(req.Body, page404Content) {
-				is404Page = true
-			}
-			if strings.Contains(req.RequestUrl, "/.") && req.StatusCode == 200 {
-				if req.ContentLength == 0 {
-					is404Page = true
-				}
-			}
-			if url.is302 {
-				if skip302 {
-					is404Page = true
-				}
-				if pkg.In_slice_string(req.Location, location404) {
-					is404Page = true
-				}
-				if !strings.Contains(req.Location, req.RequestUrl+payload+"/") {
-					location404 = append(location404, req.Location)
-					is404Page = true
-				}
-			}
-
-			if !is404Page {
-				for _, title := range other200Title {
-					if len(url.title) > 2 && url.title == title {
-						is404Page = true
-					}
-				}
-				for _, len := range other200Contentlen {
-					reqlenabs := req.ContentLength - len
-					if reqlenabs < 0 {
-						reqlenabs = -reqlenabs
-					}
-					if reqlenabs <= 5 {
-						is404Page = true
-					}
-				}
-				other200Title = append(other200Title, url.title)
-				other200Contentlen = append(other200Contentlen, req.ContentLength)
-				if !is404Page {
-					// 基于200页面文件扫描指纹添加
+		if (pkg.StringInSlice("/1.asp", path) && pkg.StringInSlice("/1.jsp", path) && pkg.StringInSlice("/2.jsp", path)) || (pkg.StringInSlice("/1.php", path) && pkg.StringInSlice("/1.jsp", path) && pkg.StringInSlice("/2.jsp", path)) || (pkg.StringInSlice("/zabbix/", path) && pkg.StringInSlice("/grafana/", path) && pkg.StringInSlice("/zentao/", path)) {
+			return nil, nil
+		}
+		ch <- struct{}{}
+		go func(payload string) {
+			if url, req, err := reqPage(u + payload); err == nil {
+				if url.is403 && (pkg.SliceInString(url.title, page403title) || pkg.SliceInString(req.Body, page403Content)) && !skip403 {
+					// 基于403页面文件扫描指纹添加
 					switch payload {
-					case "/manager/html":
-						if req.StatusCode == 401 && req.Header.Get("Www-Authenticate") != "" {
-							technologies = append(technologies, "Apache Tomcat")
-						}
-					case "/console/login/LoginForm.jsp":
-						if req.StatusCode == 200 && strings.Contains(req.Body, "Oracle WebLogic Server Administration Console") {
-							technologies = append(technologies, "weblogic")
-						}
-					case "/wls-wsat/CoordinatorPortType", "/_async/AsyncResponseService":
-						if req.StatusCode == 200 && strings.Contains(req.Body, "ws_utc") {
-							technologies = append(technologies, "weblogic")
-						}
-					case "/seeyon/":
-						if strings.Contains(req.Body, "/seeyon/common/") {
-							technologies = append(technologies, "seeyon")
-						}
-					case "/admin/":
-						if strings.Contains(req.Body, "pass") || strings.Contains(req.Body, "Pass") || strings.Contains(req.Body, "PASS") {
-							technologies = append(technologies, "admin登录页")
-						}
-					case "/zabbix/":
-						if strings.Contains(req.Body, "www.zabbix.com") {
-							technologies = append(technologies, "zabbix")
-						}
-					case "/grafana/":
-						if strings.Contains(req.Body, "grafana-app") {
-							technologies = append(technologies, "Grafana")
-						}
-					case "/zentao/":
-						if strings.Contains(req.Body, "zentao/theme") {
-							technologies = append(technologies, "zentao")
-						}
-					case "/actuator", "/actuator/archaius", "/actuator/auditevents", "/actuator/autoconfig", "/actuator/bindings", "/actuator/caches", "/actuator/channels", "/actuator/conditions", "/actuator/configprops", "/actuator/env", "/actuator/env.json", "/actuator/gateway/globalfilters", "/actuator/gateway/routefilters", "/actuator/gateway/routes", "/actuator/health", "/actuator/health.json", "/actuator/heapdump", "/actuator/hystrix.stream", "/actuator/integrationgraph", "/actuator/mappings", "/actuator/metrics", "/actuator/routes", "/actuator/scheduledtasks", "/actuator/service-registry":
-						technologies = append(technologies, "Spring")
+					case "/Runtime/Logs/":
+						technologies = append(technologies, "ThinkPHP")
 					}
 					path = append(path, payload)
 				}
+				if !pkg.IntInSlice(req.StatusCode, page200CodeList) {
+					is404Page = true
+				}
+				if url.isBackUpPath {
+					if !url.isBackUpPage {
+						is404Page = true
+					}
+				}
+				if pkg.SliceInString(url.title, page404Title) {
+					is404Page = true
+				}
+				if pkg.SliceInString(req.Body, page404Content) {
+					is404Page = true
+				}
+				if strings.Contains(req.RequestUrl, "/.") && req.StatusCode == 200 {
+					if req.ContentLength == 0 {
+						is404Page = true
+					}
+				}
+				if url.is302 {
+					if skip302 {
+						is404Page = true
+					}
+					if pkg.SliceInString(req.Location, location404) {
+						is404Page = true
+					}
+					if !strings.HasSuffix(req.Location, payload+"/") {
+						location404 = append(location404, req.Location)
+						is404Page = true
+					}
+				}
 
+				if !is404Page {
+					for _, title := range other200Title {
+						if len(url.title) > 2 && url.title == title {
+							is404Page = true
+						}
+					}
+					for _, len := range other200Contentlen {
+						reqlenabs := req.ContentLength - len
+						if reqlenabs < 0 {
+							reqlenabs = -reqlenabs
+						}
+						if reqlenabs <= 5 {
+							is404Page = true
+						}
+					}
+					other200Title = append(other200Title, url.title)
+					other200Contentlen = append(other200Contentlen, req.ContentLength)
+					if !is404Page {
+						// 基于200页面文件扫描指纹添加
+						switch payload {
+						case "/manager/html":
+							if req.StatusCode == 401 && req.Header.Get("Www-Authenticate") != "" {
+								technologies = append(technologies, "Apache Tomcat")
+							}
+						case "/console/login/LoginForm.jsp":
+							if req.StatusCode == 200 && strings.Contains(req.Body, "Oracle WebLogic Server Administration Console") {
+								technologies = append(technologies, "weblogic")
+							}
+						case "/wls-wsat/CoordinatorPortType", "/_async/AsyncResponseService":
+							if req.StatusCode == 200 && strings.Contains(req.Body, "ws_utc") {
+								technologies = append(technologies, "weblogic")
+							}
+						case "/seeyon/":
+							if strings.Contains(req.Body, "/seeyon/common/") {
+								technologies = append(technologies, "seeyon")
+							}
+						case "/admin/":
+							if strings.Contains(req.Body, "pass") || strings.Contains(req.Body, "Pass") || strings.Contains(req.Body, "PASS") {
+								technologies = append(technologies, "admin登录页")
+							}
+						case "/zabbix/":
+							if strings.Contains(req.Body, "www.zabbix.com") {
+								technologies = append(technologies, "zabbix")
+							}
+						case "/grafana/":
+							if strings.Contains(req.Body, "grafana-app") {
+								technologies = append(technologies, "Grafana")
+							}
+						case "/zentao/":
+							if strings.Contains(req.Body, "zentao/theme") {
+								technologies = append(technologies, "zentao")
+							}
+						case "/actuator", "/actuator/archaius", "/actuator/auditevents", "/actuator/autoconfig", "/actuator/bindings", "/actuator/caches", "/actuator/channels", "/actuator/conditions", "/actuator/configprops", "/actuator/env", "/actuator/env.json", "/actuator/gateway/globalfilters", "/actuator/gateway/routefilters", "/actuator/gateway/routes", "/actuator/health", "/actuator/health.json", "/actuator/heapdump", "/actuator/hystrix.stream", "/actuator/integrationgraph", "/actuator/mappings", "/actuator/metrics", "/actuator/routes", "/actuator/scheduledtasks", "/actuator/service-registry":
+							technologies = append(technologies, "Spring")
+						}
+						path = append(path, payload)
+					}
+				}
+			} else {
+				errorTimes += 1
 			}
-		} else {
-			errorTimes += 1
-		}
-		//wg.Done()
-		//	time.Sleep(time.Duration(1) * time.Second)
-		//	<-ch
-		//}(payload)
+			<-time.After(time.Duration(1000) * time.Millisecond)
+			<-ch
+		}(payload)
 	}
 	return path, technologies
 }
