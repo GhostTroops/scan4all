6776631238c8ffd3f2632a37b2fb13007f27fd4b
51pwn <51pwn@51pwn.com>
2022-06-20 23:12:03 +0800

    性能优化 2.2.0

--- pkg/hydra/runner.go
+++ pkg/hydra/runner.go
@@ -5,7 +5,7 @@ import "log"
 // 密码破解
 func Start(IPAddr string, Port int, Protocol string) {
 	authInfo := NewAuthInfo(IPAddr, Port, Protocol)
-	crack := NewCracker(authInfo, false, 32)
+	crack := NewCracker(authInfo, false, 16)
 	log.Println("[hydra]->开始对%v:%v[%v]进行暴力破解，字典长度为：%d", IPAddr, Port, Protocol, crack.Length())
 	go crack.Run()
 }
