2079ee7530615c94634752fd312d7a6ee19e98de
51pwn <51pwn@51pwn.com>
2022-06-30 23:33:49 +0800

    fix Elasticsearch url双斜杠导致无法保存的bug 2022-06-30 23:33:1656603229

--- pkg/sv2es.go
+++ pkg/sv2es.go
@@ -7,19 +7,24 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strconv"
 )
 
 var n1 int
 var nThreads chan struct{}
 var esUrl string
-var enableEsSv = GetVal("enableEsSv")
+var enableEsSv string
 
 func init() {
+	enableEsSv = GetVal("enableEsSv")
 	if "true" == enableEsSv {
 		esUrl = GetVal("esUrl")
 		n1, _ = strconv.Atoi(GetValByDefault("esthread", "4"))
+		//log.Println("es 初始化线程数 = ", n1)
 		nThreads = make(chan struct{}, n1)
+	} else {
+		Log(mData)
 	}
 }
 
@@ -39,12 +44,14 @@ func SendReq(data1 interface{}, id, szType string) {
 	if "true" != enableEsSv {
 		return
 	}
-	data, err := json.Marshal(data1)
+	//log.Println("enableEsSv = ", enableEsSv, " id= ", id, " type = ", szType)
+	data, _ := json.Marshal(data1)
 	nThreads <- struct{}{}
 	defer func() {
 		<-nThreads
 	}()
-	req, err := http.NewRequest("POST", esUrl+"/"+szType+"_index/_doc/"+id, bytes.NewReader(data))
+	url := esUrl + szType + "_index/_doc/" + url.QueryEscape(id)
+	req, err := http.NewRequest("POST", url, bytes.NewReader(data))
 	if err != nil {
 		Log(fmt.Sprintf("%s error %v", id, err))
 		return
@@ -59,6 +66,7 @@ func SendReq(data1 interface{}, id, szType string) {
 	req.Close = true
 
 	resp, err := http.DefaultClient.Do(req)
+	//log.Println("url = ", url)
 	if resp != nil {
 		defer func() {
 			err := resp.Body.Close() // resp 可能为 nil，不能读取 Body
@@ -75,6 +83,8 @@ func SendReq(data1 interface{}, id, szType string) {
 	body, err := ioutil.ReadAll(resp.Body)
 	if nil == err {
 		Log(body)
+	} else {
+		Log(err)
 	}
 	//_, err = io.Copy(ioutil.Discard, resp.Body) // 手动丢弃读取完毕的数据
 	// json.NewDecoder(resp.Body).Decode(&data)
