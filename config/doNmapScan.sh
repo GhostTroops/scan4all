53f3b691b520255a2f6314c134c001eb580c6391
51pwn <51pwn@51pwn.com>
2022-07-14 12:03:06 +0800

    reset nmap bash shell 2022-07-14 12:03:1657771386

--- config/doNmapScan.sh
+++ config/doNmapScan.sh
@@ -3,9 +3,10 @@ XRate=5000
 function doMasScan {
     if [[ -f $1 ]] ; then
         # -F --top-ports=65535
-        echo $PPSSWWDD|sudo -S nmap  -p 80,443 -n --unique --resolve-all -Pn -sS --min-hostgroup 64 --max-retries 0 --host-timeout 10m --script-timeout 3m --version-intensity 9 --min-rate ${XRate} -T4  -iL $1 -oX $2
+        #  -p 80,443
+        echo $PPSSWWDD|sudo -S nmap -F --top-ports=65535 -n --unique --resolve-all -Pn -sS --min-hostgroup 64 --max-retries 0 --host-timeout 10m --script-timeout 3m --version-intensity 9 --min-rate ${XRate} -T4  -iL $1 -oX $2
     else
-        echo $PPSSWWDD|sudo -S nmap  -p 80,443 -n --unique --resolve-all -Pn -sS --min-hostgroup 64 --max-retries 0 --host-timeout 10m --script-timeout 3m --version-intensity 9 --min-rate ${XRate} -T4  $1 -oX $2
+        echo $PPSSWWDD|sudo -S nmap -F --top-ports=65535 -n --unique --resolve-all -Pn -sS --min-hostgroup 64 --max-retries 0 --host-timeout 10m --script-timeout 3m --version-intensity 9 --min-rate ${XRate} -T4  $1 -oX $2
     fi
 }
 doMasScan $1 $2
