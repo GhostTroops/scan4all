20f7d031c46495074ca80846b3be240471b01be3
51pwn <51pwn@51pwn.com>
2022-07-21 22:20:09 +0800

    up nuclei-temlates and xry 2022-07-21

--- .github/up.sh
+++ .github/up.sh
@@ -1,19 +1,19 @@
-cd $HOME/MyWork/nuclei-templates
-git pull
+upGit xray
+upGit nuclei-templates
 cd $HOME/MyWork/scan4all
 cat ./go.mod|grep projectdiscovery|grep -E "subfinder|nuclei|wappalyzergo"|awk '{print $1}'|xargs -I % go get -u %
 
+cp -rf $HOME/MyWork/xray/pocs/*.yml $HOME/MyWork/scan4all/pocs_yml/ymlFiles/
 ls ../nuclei-templates|xargs -I % cp -rf ../nuclei-templates/% config/nuclei-templates/
+echo "start 静态go.mod去除不相关依赖"
 go mod tidy
+echo "更新 vendor "
 go mod vendor
-# 工具静态分析代码实现
+echo "工具静态分析代码实现"
 go vet
-
-cat ./pkg/fingerprint/dicts/eHoleFinger.json|jq ".fingerprint[].cms"|wc -l
-cat ./pkg/fingerprint/dicts/localFinger.json|jq ".fingerprint[].cms"|wc -l
-cat ./pkg/fingerprint/dicts/fg.json|jq ".[].kind"|wc -l
-git add config/nuclei-templates
+#cat ./pkg/fingerprint/dicts/eHoleFinger.json|jq ".fingerprint[].cms"|wc -l
+#cat ./pkg/fingerprint/dicts/localFinger.json|jq ".fingerprint[].cms"|wc -l
+#cat ./pkg/fingerprint/dicts/fg.json|jq ".[].kind"|wc -l
+git add config/nuclei-templates pocs_yml/ymlFiles
 git status
 go build
-
-
