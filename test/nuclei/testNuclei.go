a19f5988482f27690607b16ba99afe0d304d099e
51pwn <51pwn@51pwn.com>
2022-07-21 13:23:53 +0800

    add CVE-2022-30525 CVE-2022-33891 CVE-2021-24284 2022-07-21

--- test/nuclei/testNuclei.go
+++ test/nuclei/testNuclei.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bytes"
 	"github.com/hktalent/scan4all/projectdiscovery/nuclei_Yaml"
+	"github.com/projectdiscovery/nuclei/v2/pkg/model/types/severity"
+	nucleiType "github.com/projectdiscovery/nuclei/v2/pkg/templates/types"
 	_ "net/http/pprof"
 	"os"
 	"sync"
@@ -39,23 +41,28 @@ func main() {
 		//	fmt.Println("debug info: \nopen http://127.0.0.1:6060/debug/pprof/\n\ngo tool pprof -seconds=10 -http=:9999 http://localhost:6060/debug/pprof/heap")
 		//	http.ListenAndServe(":6060", nil)
 		//}()
+		h01 := []severity.Severity{severity.Critical, severity.High, severity.Medium}
+		//data1, err := json.Marshal(h01)
+		//if nil == err {
+		//	log.Printf("%+v", string(data1))
+		//}
 		buf := bytes.Buffer{}
 		var wg sync.WaitGroup
 		wg.Add(1)
 		buf.WriteString("http://192.168.10.31:8888\n")
 		pwd, _ := os.Getwd()
-		m1 := map[string]interface{}{"Severities": "critical,high,medium", "EnableProgressBar": false, "UpdateTemplates": false, "Templates": []string{pwd + "/config/nuclei-templates"}, "TemplatesDirectory": pwd + "/config/nuclei-templates", "NoUpdateTemplates": true}
+		m1 := map[string]interface{}{"Severities": h01, "EnableProgressBar": false, "UpdateTemplates": false, "Templates": []string{pwd + "/config/nuclei-templates"}, "TemplatesDirectory": pwd + "/config/nuclei-templates", "NoUpdateTemplates": true}
 		go DoNuclei(&buf, &wg, &m1)
 
 		buf1 := bytes.Buffer{}
 		buf1.WriteString("http://pms.yx4.me\n")
 		wg.Add(1)
-		m2 := map[string]interface{}{"Severities": "critical,high,medium", "EnableProgressBar": false, "Protocols": "http", "UpdateTemplates": false, "Templates": []string{pwd + "/config/nuclei-templates"}, "TemplatesDirectory": pwd + "/config/nuclei-templates", "NoUpdateTemplates": true}
+		m2 := map[string]interface{}{"Severities": h01, "EnableProgressBar": false, "Protocols": []nucleiType.ProtocolType{nucleiType.HTTPProtocol}, "UpdateTemplates": false, "Templates": []string{pwd + "/config/nuclei-templates"}, "TemplatesDirectory": pwd + "/config/nuclei-templates", "NoUpdateTemplates": true}
 		go DoNuclei(&buf1, &wg, &m2)
 
 		buf2 := bytes.Buffer{}
-		buf2.WriteString("http://192.168.10.240\n")
-		m3 := map[string]interface{}{"Severities": "critical,high,medium", "EnableProgressBar": false, "Protocols": "network", "UpdateTemplates": false, "Templates": []string{pwd + "/config/nuclei-templates"}, "TemplatesDirectory": pwd + "/config/nuclei-templates", "NoUpdateTemplates": true}
+		buf2.WriteString("https://kb.bugscan.net\n")
+		m3 := map[string]interface{}{"Severities": h01, "EnableProgressBar": false, "Protocols": []nucleiType.ProtocolType{nucleiType.NetworkProtocol}, "UpdateTemplates": false, "Templates": []string{pwd + "/config/nuclei-templates/51pwn"}, "TemplatesDirectory": pwd + "/config/nuclei-templates", "NoUpdateTemplates": true}
 		wg.Add(1)
 		go DoNuclei(&buf2, &wg, &m3)
 		wg.Wait()
