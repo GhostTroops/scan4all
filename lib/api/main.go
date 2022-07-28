84d81740ad4fbfb0d4617d7151535cc4587dcfe3
51pwn <51pwn@51pwn.com>
2022-07-28 17:50:20 +0800

    1、调整main 2、重新fix nuclei #2301 之bug，否则nuclei会爆内存错误、异常退出；期待官方尽快merge我的PR 2022-07-28

---
+++ lib/api/main.go
@@ -0,0 +1,74 @@
+package api
+
+import (
+	"encoding/json"
+	"github.com/hktalent/scan4all/lib/util"
+	"github.com/hktalent/scan4all/pkg/hydra"
+	naaburunner "github.com/hktalent/scan4all/pkg/naabu/v2/pkg/runner"
+	"github.com/projectdiscovery/gologger"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"runtime"
+)
+
+// 逐步实现支持 多实例 接口 运行
+func StartScan(oOpts *map[string]interface{}) {
+	util.DoSyncFunc(func() {
+		options := naaburunner.ParseOptions()
+		// 压入外部参数
+		if nil != oOpts {
+			// 指定覆盖
+			data, err := json.Marshal(oOpts)
+			if nil == err && 0 < len(data) {
+				err := json.Unmarshal(data, options)
+				if nil != err {
+					log.Println("oOpts err ", err)
+				}
+			}
+		}
+		if false == options.Debug && false == options.Verbose {
+			// disable standard logger (ref: https://github.com/golang/go/issues/19895)
+			log.SetFlags(0)
+			log.SetOutput(io.Discard)
+		}
+		// 这里需要解决、优化为非单实例 模式
+		util.G_Options = options
+		if runtime.GOOS == "windows" {
+			options.NoColor = true
+		}
+
+		naabuRunner, err := naaburunner.NewRunner(options)
+		if err != nil {
+			gologger.Fatal().Msgf("naaburunner.NewRunner Could not create runner: %s\n", err)
+		}
+		noScan := util.GetValAsBool("noScan")
+
+		// 直接使用 nmap xml结果文件
+		if hydra.DoNmapWithFile(options.HostsFile, &naaburunner.Naabubuffer) {
+			os.Setenv("noScan", "true")
+			naabuRunner.Close()
+		} else if noScan {
+			s1, err := naabuRunner.MergeToFile()
+			if nil == err {
+				data, err := ioutil.ReadFile(s1)
+				if nil == err {
+					naaburunner.Naabubuffer.Write(data)
+				}
+			}
+			naabuRunner.Close()
+		} else {
+			gologger.Info().Msg("Port scan starting....")
+			err = naabuRunner.RunEnumeration()
+			if err != nil {
+				gologger.Fatal().Msgf("Could not run enumeration: %s\n", err)
+			}
+			gologger.Info().Msg("Port scan over,web scan starting")
+		}
+		err = naabuRunner.Httpxrun()
+		if err != nil {
+			gologger.Fatal().Msgf("naabuRunner.Httpxrun Could not run httpRunner: %s\n", err)
+		}
+	})
+}
