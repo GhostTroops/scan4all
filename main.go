2b2ca0f68185e85cab27bea581e949ad25ea4cf6
51pwn <51pwn@51pwn.com>
2022-07-28 17:50:20 +0800

    1、调整main 2、重新fix nuclei #2301 之bug，否则nuclei会爆内存错误、异常退出；期待官方尽快merge我的PR 2022-07-28

--- main.go
+++ main.go
@@ -3,12 +3,8 @@ package main
 import (
 	"embed"
 	"fmt"
+	"github.com/hktalent/scan4all/lib/api"
 	"github.com/hktalent/scan4all/lib/util"
-	"github.com/hktalent/scan4all/pkg/hydra"
-	naaburunner "github.com/hktalent/scan4all/pkg/naabu/v2/pkg/runner"
-	"github.com/projectdiscovery/gologger"
-	"io"
-	"io/ioutil"
 	"log"
 	"math/rand"
 	"net/http"
@@ -41,10 +37,8 @@ func main() {
 		// 程序都结束了，没有必要清理内存了
 		// fingerprint.ClearData()
 	}()
-
-	options := naaburunner.ParseOptions()
 	szTip := ""
-	if options.Debug && util.GetValAsBool("enablDevDebug") {
+	if util.GetValAsBool("enablDevDebug") {
 		// debug 优化时启用///////////////////////
 		go func() {
 			szTip = "Since you started http://127.0.0.1:6060/debug/pprof/ with -debug, close the program with: control + C"
@@ -53,50 +47,9 @@ func main() {
 		}()
 		//////////////////////////////////////////*/
 	}
-	if false == options.Debug && false == options.Verbose {
-		// disable standard logger (ref: https://github.com/golang/go/issues/19895)
-		log.SetFlags(0)
-		log.SetOutput(io.Discard)
-	}
-	util.G_Options = options
-	if runtime.GOOS == "windows" {
-		options.NoColor = true
-	}
-	naabuRunner, err := naaburunner.NewRunner(options)
-	if err != nil {
-		gologger.Fatal().Msgf("naaburunner.NewRunner Could not create runner: %s\n", err)
-	}
-	noScan := util.GetValAsBool("noScan")
+	api.StartScan(nil)
 
-	// 直接使用 nmap xml结果文件
-	if hydra.DoNmapWithFile(options.HostsFile, &naaburunner.Naabubuffer) {
-		os.Setenv("noScan", "true")
-		naabuRunner.Close()
-	} else if noScan {
-		s1, err := naabuRunner.MergeToFile()
-		if nil == err {
-			data, err := ioutil.ReadFile(s1)
-			if nil == err {
-				naaburunner.Naabubuffer.Write(data)
-			}
-		}
-		naabuRunner.Close()
-	} else {
-		gologger.Info().Msg("Port scan starting....")
-		err = naabuRunner.RunEnumeration()
-		if err != nil {
-			gologger.Fatal().Msgf("Could not run enumeration: %s\n", err)
-		}
-		gologger.Info().Msg("Port scan over,web scan starting")
-	}
-	err = naabuRunner.Httpxrun()
-	if err != nil {
-		gologger.Fatal().Msgf("naabuRunner.Httpxrun Could not run httpRunner: %s\n", err)
-	}
 	log.Printf("wait for all threads to end\n%s", szTip)
 	util.Wg.Wait()
 	util.StopAll()
-	if !noScan {
-		naabuRunner.Close()
-	}
 }
