360864f8e72f380473c6764df6289d5209aac43d
hktalent <18223385+hktalent@users.noreply.github.com>
2022-08-04 21:29:48 +0800

    完善es脚步 2022-08-04

--- config/initEs.sh
+++ config/initEs.sh
@@ -1,5 +1,11 @@
+mkdir "data"
 mkdir "logs"
-docker run --restart=always --ulimit nofile=65536:65536 -p 9200:9200 -p 9300:9300 -d --name es1 -v $PWD/logs:/usr/share/elasticsearch/logs -v $PWD/config/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml -v $PWD/config/jvm.options:/usr/share/elasticsearch/config/jvm.options  -v $PWD/data:/usr/share/elasticsearch/data  hktalent/elasticsearch:7.16.2
+docker run --restart=always --ulimit nofile=65536:65536 \
+  -p 9200:9200 -p 9300:9300 -d --name es1 \
+  -v $PWD/logs:/usr/share/elasticsearch/logs \
+  -v $PWD/config/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml \
+  -v $PWD/config/jvm.options:/usr/share/elasticsearch/config/jvm.options  \
+  -v $PWD/data:/usr/share/elasticsearch/data  hktalent/elasticsearch:7.16.2
 echo "Wait for es to start before running the following script"
 docker logs -f es1
 echo "Wait for es to start before running the following script"
