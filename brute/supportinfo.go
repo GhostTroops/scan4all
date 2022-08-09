587601e245ff09060f61b7b31a7c8bf221497f40
hktalent <18223385+hktalent@users.noreply.github.com>
2022-08-09 08:10:33 +0800

    1、up PoCs 2、add supply chain 2022-08-09

---
+++ brute/supportinfo.go
@@ -0,0 +1,22 @@
+package brute
+
+import (
+	_ "embed"
+	"github.com/hktalent/scan4all/lib/util"
+	"regexp"
+	"strings"
+)
+
+//go:embed dicts/cprt.txt
+var supplyChainPrefix string
+
+//go:embed dicts/softc.txt
+var supplyChainEndstr string
+
+func init() {
+	util.RegInitFunc(func() {
+		p1 := "((" + strings.Join(strings.Split(strings.TrimSpace(supplyChainPrefix), "\n"), ")|(") + "))\\s*[:：]\\s*"
+		p2 := "((" + strings.Join(strings.Split(strings.TrimSpace(supplyChainEndstr), "\n"), ")|(") + "))"
+		util.SupplyChainReg = regexp.MustCompile(p1 + p2)
+	})
+}
