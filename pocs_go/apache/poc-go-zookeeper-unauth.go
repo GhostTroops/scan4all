342b5a912a08d83bdd900e4e57255559b0dd8848
hktalent <18223385+hktalent@users.noreply.github.com>
2022-09-01 22:01:11 +0800

    Up PoCs 2022-09-01

---
+++ pocs_go/apache/poc-go-zookeeper-unauth.go
@@ -0,0 +1,25 @@
+package apache
+
+import (
+	"github.com/hktalent/scan4all/lib/socket"
+	"strings"
+)
+
+// ZookeeperUnauthority zookeeper 未授权
+//
+//	addr := args.Host + ":2181"
+func ZookeeperUnauthority(szUrl string) bool {
+	payload := "envidddfdsfsafafaerwrwerqwe"
+	x1 := socket.NewCheckTarget(szUrl, "tcp", 10)
+	defer x1.Close()
+	_, err := x1.ConnTarget()
+	if err != nil {
+		return false
+	}
+	x1.WriteWithFlush(payload)
+	s1 := *x1.ReadAll2Str()
+	if "" != s1 && -1 < strings.Index(s1, "Environment") {
+		return true
+	}
+	return false
+}
