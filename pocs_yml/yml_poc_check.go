f254ee2a2da86dd45ca385c40ae92d1208cc091c
hktalent <18223385+hktalent@users.noreply.github.com>
2022-09-18 06:25:29 +0800

    Optimize filefuzz 2022-09-18

--- pocs_yml/yml_poc_check.go
+++ pocs_yml/yml_poc_check.go
@@ -7,6 +7,7 @@ import (
 	common_structs "github.com/hktalent/scan4all/pocs_yml/pkg/common/structs"
 	xray_requests "github.com/hktalent/scan4all/pocs_yml/pkg/xray/requests"
 	"github.com/hktalent/scan4all/pocs_yml/utils"
+	"net/url"
 	"time"
 )
 
@@ -14,9 +15,11 @@ import (
 var Pocs embed.FS
 
 func Check(target string, ceyeapi string, ceyedomain string, proxy string, pocname string) []string {
-	if util.TestRepeat(target, ceyeapi, ceyedomain, proxy, pocname, "yml_poc_check") {
+	u01, _ := url.Parse(target)
+	if util.TestRepeat(u01.Host, ceyeapi, ceyedomain, proxy, pocname, "yml_poc_check") {
 		return []string{}
 	}
+	//fmt.Println(u01.Host + " |||" + pocname)
 	common_structs.InitReversePlatform(ceyeapi, ceyedomain)
 	_ = xray_requests.InitHttpClient(10, proxy, time.Duration(5)*time.Second)
 	xrayPocs := utils.LoadMultiPoc(Pocs, pocname)
