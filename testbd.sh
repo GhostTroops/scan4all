b3452bc91aadefb54c2eb5ee38470a0a19a7c69c
hktalent <18223385+hktalent@users.noreply.github.com>
2022-09-17 17:04:29 +0800

    fixed bug:Configs.ConfigJsonMap.Exploit.Logs is "<nil>" #111 By @hktalent 2022-09-17

--- testbd.sh
+++ testbd.sh
@@ -3,3 +3,5 @@ rm -rf vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/interac
 wget -c -O vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/http/request.go https://github.com/hktalent/scan4all/raw/main/vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/http/request.go
 wget -c -O vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/interactsh/interactsh.go https://github.com/hktalent/scan4all/raw/main/vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/interactsh/interactsh.go
 go build
+
+find . -name ".DS_Store" -delete
