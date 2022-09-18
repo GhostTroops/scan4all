c256e2eaf7057f26cf738840d43c120fe1b31549
hktalent <18223385+hktalent@users.noreply.github.com>
2022-09-18 21:54:50 +0800

    fix go.mod auto clear bug

--- fixMod.sh
+++ fixMod.sh
@@ -3,4 +3,7 @@ go get github.com/projectdiscovery/retryabledns@v1.0.13
 go get "github.com/projectdiscovery/stringsutil@v0.0.0-20220612082425-0037ce9f89f3"
 go get "github.com/projectdiscovery/ipranger@v0.0.3-0.20210831161617-ac80efae0961"
 
+git checkout vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/http/request.go
+git checkout vendor/github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/interactsh/interactsh.go
+
 git checkout vendor/github.com/projectdiscovery/nuclei/v2
