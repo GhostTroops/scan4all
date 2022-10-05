c902867e4467d0e2c51d0c0abf6f4731e700cc1c
hktalent <18223385+hktalent@users.noreply.github.com>
2022-10-05 14:31:14 +0800

    up 2022-10-05

--- lib/crawlergo/test/main.go
+++ lib/crawlergo/test/main.go
@@ -2,17 +2,71 @@ package main
 
 import (
 	"context"
+	"github.com/chromedp/chromedp"
 	pkg "github.com/hktalent/scan4all/lib/crawlergo"
 	"log"
+	"time"
 )
 
 func main() {
+	//ctx := context.Background()
+	//x1 := pkg.GetChromedpInstace(&ctx)
+	////n1 := 15 * time.Second
+	//n1 := 1000 * time.Second
+	//
+	//// Username already used
+	////x1.DoUrl("https://google.com", &map[string]interface{}{"cookie": "xxx"}, nil)
+	//if err := x1.DoUrl("https://account.proton.me/signup?plan=free&billing=12&minimumCycle=12&currency=undefined&language=en", &map[string]interface{}{}, &n1, func() *chromedp.Tasks {
+	//	szPswd := ""
+	//	return &chromedp.Tasks{
+	//		chromedp.SetValue(`#email`, "hk_994", chromedp.ByID),
+	//		chromedp.SetValue(`#password`, szPswd, chromedp.ByID),
+	//		chromedp.SetValue(`#repeat-password`, szPswd, chromedp.ByID),
+	//		chromedp.Click("main > div.sign-layout-main-content > form > button", chromedp.NodeReady),
+	//		chromedp.Click("#html_element", chromedp.ByID),
+	//	}
+	//	// next:hk_994
+	//	// main > div.sign-layout-main-content > form > button
+	//	// 跳过手机号码
+	//	// /html/body/div[1]/div[3]/div/div/main/div[2]/form/button[2]
+	//}); err != nil {
+	//	log.Println(err)
+	//}
+	////defer x1.Close()
+
+	RegGithub()
+}
+
+// https://github.com/signup?ref_cta=Sign+up&ref_loc=header+logged+out&ref_page=%2F&source=header-home
+func RegGithub() {
 	ctx := context.Background()
 	x1 := pkg.GetChromedpInstace(&ctx)
 	//n1 := 15 * time.Second
-	//x1.DoUrl("https://google.com", &map[string]interface{}{"cookie": "xxx"}, nil)
-	if err := x1.DoUrl("https://www.baidu.com", &map[string]interface{}{}, nil); err != nil {
+	n1 := 1000 * time.Second
+
+	if err := x1.DoUrl("https://github.com/signup?ref_cta=Sign+up&ref_loc=header+logged+out&ref_page=%2F&source=header-home", &map[string]interface{}{}, &n1, func() *chromedp.Tasks {
+		szPswd := ""
+		return &chromedp.Tasks{
+			chromedp.SetValue(`#email`, "hk_994@proton.me", chromedp.ByID),
+			chromedp.Sleep(2 * time.Second),
+			chromedp.Click("//*[@id=\"email-container\"]/div[2]/button", chromedp.NodeReady),
+			chromedp.Sleep(2 * time.Second),
+			chromedp.SetValue(`#password`, szPswd, chromedp.ByID),
+			chromedp.Sleep(2 * time.Second),
+			chromedp.Click(`document.querySelector("#password-container > div.d-flex.flex-items-center.flex-column.flex-sm-row > button")`, chromedp.ByJSPath),
+			chromedp.Sleep(2 * time.Second),
+			chromedp.SetValue(`#login`, "hk999999", chromedp.ByID),
+			chromedp.Sleep(2 * time.Second),
+			chromedp.SetValue(`#opt_in`, "n", chromedp.ByID),
+			chromedp.Sleep(2 * time.Second),
+			chromedp.Click(`document.querySelector("#opt-in-container > div.d-flex.flex-items-center.flex-column.flex-sm-row > button")`, chromedp.ByJSPath),
+		}
+		// next:hk_994
+		// main > div.sign-layout-main-content > form > button
+		// 跳过手机号码
+		// /html/body/div[1]/div[3]/div/div/main/div[2]/form/button[2]
+	}); err != nil {
 		log.Println(err)
 	}
-	defer x1.Close()
+	//defer x1.Close()
 }
