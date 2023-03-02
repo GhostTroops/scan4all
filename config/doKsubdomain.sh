c91bc27d05e2f09cc82c25d7210436d49cd16bc8
hktalent <18223385+hktalent@users.noreply.github.com>
2023-03-02 17:33:49 +0800

    add TPALL 2023-03-02

--- config/doKsubdomain.sh
+++ config/doKsubdomain.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 function doMasScan {
     if [[ -f $1 ]] ; then
-        echo $PPSSWWDD| sudo -S ksubdomain enum -b 5M --dl $1  -f $HOME/MyWork/scan4all/config/database/subdomain.txt -o $HOME/MyWork/scan4all/atckData/$2
+        echo $PPSSWWDD| sudo -S ksubdomain enum -b 5M --dl $1  -f $HOME/MyWork/scan4all/config/database/subdomain.txt -o $HOME/MyWork/scan4all/atckData/$2.txt
     else
-        echo $PPSSWWDD| sudo -S ksubdomain enum -b 5M -d $1  -f $HOME/MyWork/scan4all/config/database/subdomain.txt -o $HOME/MyWork/scan4all/atckData/$2
+        echo $PPSSWWDD| sudo -S ksubdomain enum -b 5M -d $1  -f $HOME/MyWork/scan4all/config/database/subdomain.txt -o $HOME/MyWork/scan4all/atckData/$2.txt
     fi
 }
 doMasScan $1 $2
